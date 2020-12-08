a32cfeec91c1061cd9dd2ff7717b7f0956ff387f
saitoCIT <75356150+saitoCIT@users.noreply.github.com>
2020-12-08 10:27:01 +0900

    Update myled.c

--- myled.c
+++ myled.c
@@ -21,78 +21,77 @@ static volatile u32 *gpio_base =  NULL;
 static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_t* pos)
 {
 	char c;
-		if(copy_from_user(&c,buf,sizeof(char)))
-					return -EFAULT;
+	if(copy_from_user(&c,buf,sizeof(char)))
+		return -EFAULT;
 
-		if(c == '0') {
-			gpio_base[10] = 1 << 25;
-			gpio_base[10] = 1 << 24;
-			gpio_base[10] = 1 << 23;
-			gpio_base[10] = 1 << 22;
-		}
-		else if(c == '1'){
-			gpio_base[7] = 1 << 25;
-			gpio_base[7] = 1 << 24;
-			gpio_base[10] = 1 << 23;
-			gpio_base[10] = 1 << 22;
-		}
-		else if(c == '2'){
-			gpio_base[7] = 1 << 25;
-			gpio_base[10] = 1 << 24;
-			gpio_base[10] = 1 << 23;
-			gpio_base[10] = 1 << 22;
-		}
-		else if(c == '3'){
-			gpio_base[10] = 1 << 25;
-			gpio_base[10] = 1 << 24;
-			gpio_base[7] = 1 << 23;
-			gpio_base[10] = 1 << 22;
-		}
-		else if(c == '4'){
-			gpio_base[7] = 1 << 25;
-			gpio_base[10] = 1 << 24;
-			gpio_base[10] = 1 << 23;
-			gpio_base[10] = 1 << 22;
-		}
-		else if (c == '5'){
-			gpio_base[7] = 1 << 25;
-			gpio_base[10] = 1 << 24;
-			gpio_base[7] = 1 << 23;
-			gpio_base[10] = 1 << 22;
-		}
-		else if (c == '6'){
-			gpio_base[7] = 1 << 25;
-			gpio_base[10] = 1 << 24;
-			gpio_base[10] = 1 << 23;
-			gpio_base[7] = 1 << 22;
-		}
-		else if (c == '7'){
-			gpio_base[10] = 1 << 25;
-			gpio_base[7] = 1 << 24;
-			gpio_base[10] = 1 << 23;
-			gpio_base[10] = 1 << 22;
-		}
-		else if (c == '8'){
-			gpio_base[10] = 1 << 25;
-			gpio_base[7] = 1 << 24;
-			gpio_base[7] = 1 << 23;
-			gpio_base[10] = 1 << 22;
-		}
-		else if (c == '9'){
-			gpio_base[7] = 1 << 25;
-			gpio_base[10] = 1 << 24;
-			gpio_base[7] = 1 << 23;
-			gpio_base[10] = 1 << 22;
-		
-		}
+	if(c == '0') {
+		gpio_base[10] = 1 << 25;
+		gpio_base[10] = 1 << 24;
+		gpio_base[10] = 1 << 23;
+		gpio_base[10] = 1 << 22;
+	}
+	else if(c == '1'){
+		gpio_base[7] = 1 << 25;
+		gpio_base[7] = 1 << 24;
+		gpio_base[10] = 1 << 23;
+		gpio_base[10] = 1 << 22;
+	}
+	else if(c == '2'){
+		gpio_base[7] = 1 << 25;
+		gpio_base[10] = 1 << 24;
+		gpio_base[10] = 1 << 23;
+		gpio_base[10] = 1 << 22;
+	}
+	else if(c == '3'){
+		gpio_base[10] = 1 << 25;
+		gpio_base[10] = 1 << 24;
+		gpio_base[7] = 1 << 23;
+		gpio_base[10] = 1 << 22;
+	}
+	else if(c == '4'){
+		gpio_base[7] = 1 << 25;
+		gpio_base[10] = 1 << 24;
+		gpio_base[10] = 1 << 23;
+		gpio_base[10] = 1 << 22;
+	}
+	else if (c == '5'){
+		gpio_base[7] = 1 << 25;
+		gpio_base[10] = 1 << 24;
+		gpio_base[7] = 1 << 23;
+		gpio_base[10] = 1 << 22;
+	}
+	else if (c == '6'){
+		gpio_base[7] = 1 << 25;
+		gpio_base[10] = 1 << 24;
+		gpio_base[10] = 1 << 23;
+		gpio_base[7] = 1 << 22;
+	}
+	else if (c == '7'){
+		gpio_base[10] = 1 << 25;
+		gpio_base[7] = 1 << 24;
+		gpio_base[10] = 1 << 23;
+		gpio_base[10] = 1 << 22;
+	}
+	else if (c == '8'){
+		gpio_base[10] = 1 << 25;
+		gpio_base[7] = 1 << 24;
+		gpio_base[7] = 1 << 23;
+		gpio_base[10] = 1 << 22;
+	}
+	else if (c == '9'){
+		gpio_base[7] = 1 << 25;
+		gpio_base[10] = 1 << 24;
+		gpio_base[7] = 1 << 23;
+		gpio_base[10] = 1 << 22;
+	}
 
-		return 1;
+	return 1;
 }
 
 static struct file_operations led_fops = {
-		.owner = THIS_MODULE,
-		.write = led_write
-		};
+	.owner = THIS_MODULE,
+	.write = led_write
+};
 
 static int __init init_mod(void)
 {
@@ -115,32 +114,32 @@ static int __init init_mod(void)
 
 	printk(KERN_INFO "%s is loaded. major:%d\n",__FILE__,MAJOR(dev));
 
-		cdev_init(&cdv, &led_fops);
-		cdv.owner = THIS_MODULE;
-		retval = cdev_add(&cdv, dev, 1);
-			if(retval < 0){
-				printk(KERN_ERR "cdev_add failed. major:%d, minor:%d\n",MAJOR(dev),MINOR(dev));
-				return retval;
-			}
+	cdev_init(&cdv, &led_fops);
+	cdv.owner = THIS_MODULE;
+	retval = cdev_add(&cdv, dev, 1);
+	if(retval < 0){
+		printk(KERN_ERR "cdev_add failed. major:%d, minor:%d\n",MAJOR(dev),MINOR(dev));
+		return retval;
+	}
 
-		cls = class_create(THIS_MODULE,"myled");
-		if(IS_ERR(cls)){
-			printk(KERN_ERR "class_create failed.");
-			return PTR_ERR(cls);
-		}
-		device_create(cls, NULL, dev, NULL, "myled%d",MINOR(dev));
+	cls = class_create(THIS_MODULE,"myled");
+	if(IS_ERR(cls)){
+		printk(KERN_ERR "class_create failed.");
+		return PTR_ERR(cls);
+	}
+	device_create(cls, NULL, dev, NULL, "myled%d",MINOR(dev));
 
-			return 0;
+	return 0;
 }
 
 static void __exit cleanup_mod(void)
 {
 	cdev_del(&cdv);
-		device_destroy(cls, dev);
-		class_destroy(cls);
-		unregister_chrdev_region(dev, 1);
-		printk(KERN_INFO "%s is unloaded. major:%d\n",__FILE__,MAJOR(dev));
-		iounmap(gpio_base);
+	device_destroy(cls, dev);
+	class_destroy(cls);
+	unregister_chrdev_region(dev, 1);
+	printk(KERN_INFO "%s is unloaded. major:%d\n",__FILE__,MAJOR(dev));
+	iounmap(gpio_base);
 }
 
 module_init(init_mod);
